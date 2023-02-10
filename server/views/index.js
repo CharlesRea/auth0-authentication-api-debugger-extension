cf8f03190c506df919107c5b2441d592e8e88637
Charles Rea <charles.rea@auth0.com>
2023-02-10 12:58:35 +0000

    Custom changes

--- server/views/index.js
+++ server/views/index.js
@@ -1,6 +1,6 @@
 module.exports = `<html lang="en">
 <head>
-  <title>Auth0 - Protocol Debugger</title>
+  <title>Auth0 - Protocol Debugger (custom)</title>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=Edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
